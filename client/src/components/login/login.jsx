b1c35a4cbaaa3e8a03eb6e3c388c621f91eb70b4
Your Name <you@example.com>
2025-01-31 12:41:48 +0530

    tailwind added

---
+++ client/src/components/login/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './login.css'
+
+function Login() {
+  return (
+    <div>
+      <h1  className="l1 text-3xl font-bold underline">
+
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default Login
